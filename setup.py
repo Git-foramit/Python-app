92e99299dad658a0f8e9d4e06e7834655b8952da
JOA1ABT <notesforamit@gmail.com>
2022-06-20 13:20:20 +0200

    Made final changes

--- setup.py
+++ setup.py
@@ -1,4 +1,6 @@
 import os
+
+import setuptools
 from setuptools import find_packages, setup
 
 with open(os.path.join(os.path.dirname(__file__), "README.md")) as readme:
@@ -10,7 +12,7 @@ setuptools.setup(
     author="Amit Joshi",
     author_email="notesforamit@gmail.com",
     description="An easy and funny example of a Python package",
-    long_description="My_own_Python_package contains a funny game for calculating the result of arithmetic operations applied to an unknow number given by the player",
+    long_description="Calculating the result of arithmetic operations applied to an unknown number given by the player",
     long_description_content_type="text/markdown",
     url="https://github.com/Git-foramit/Python-app",
     classifiers=[
